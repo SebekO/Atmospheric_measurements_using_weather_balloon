92ee835455d400cf91b4d8949e0dce44cbb64999
SebekO <sebastian.owarzany@gmail.com>
2018-11-30 01:10:12 +0100

    Add files via upload

--- DGS_master_update/src/DGS.cpp
+++ DGS_master_update/src/DGS.cpp
@@ -408,12 +408,11 @@ long DGS::getTime(char s)
 }
 String DGS::getId(char i)
 {
-	static int z=(eepromStr[1].length()-1);
   if (i == 'i'){
-	  return (eepromStr[1].remove(z));
+	  return (eepromStr[1].remove(12));
 	  }
   else {
-	  return (eepromStr[1].remove(z));
+	  return (eepromStr[1].remove(12));
 	  }
 }
 
